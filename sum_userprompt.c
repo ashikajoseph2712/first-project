9859b612749374c600bc68fe01a989955c017999
ashikajoseph2712 <ashikajoseph2712@gmail.com>
2025-12-07 20:58:04 +0530

    Create sum_userprompt.c

---
+++ sum_userprompt.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int x,y,sum;
+printf("Enter first number= ");
+scanf("%d",&x);
+printf("Enter second number");
+scanf("%d",&y);
+ssum=x+y;
+printf("Sum = %d",sum);
+return 0;
+}
